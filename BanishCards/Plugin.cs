36c43e0d1885b22a8375bb52629ac1593e82ae54
Agustín Butrico <agustinbutrico@gmail.com>
2025-06-29 03:13:53 -0300

    Super basic set up.

---
+++ BanishCards/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BanishCards
+{
+    [BepInPlugin("AgusBut.BanishCards", "BanishCards", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin Instance { get; private set; }
+        public static BepInEx.Logging.ManualLogSource Log { get; private set; }
+
+        // Máximo configurable de cartas a desaparecer
+        private ConfigEntry<KeyCode> banishCardsMax;
+        public ConfigEntry<KeyCode> BanishCardsMax => banishCardsMax;
+        private void Awake()
+        {
+            Instance = this;
+            Log = base.Logger;
+
+            Logger.LogInfo("Loading [BanishCards 1.0.0]");
+        }
+    }
+}
